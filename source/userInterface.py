43f39f6d1ddf3bcf1ba6fdde1c4a2767986ac33d
Dominikkie <kienle.dominik.03@gmail.com>
2024-08-07 23:05:00 +0200

    Interface weitergebaut bis Eingabe Passwort, Erstellen eines Accounts, fehlt noch anzeigen der Passwörter und speichern der Accounts, dann rollt das Interface

--- source/userInterface.py
+++ source/userInterface.py
@@ -3,22 +3,9 @@ import curses
 import csv
 #from account import Account
 
-accounts = ["Tim", "Justin", "Dominik"]
+accounts = ["Choose an Account","Tim", "Justin", "Dominik"]
 
-data = [
-    {
-        "category": "category1",
-        "plattform": "Netflix",
-        "password": "HAHAHA1234_Ü",
-        "note 1": "Das Password ist falsch"
-    },
-    {
-        "category": "category1",
-        "plattform": "LeagueOfLegends",
-        "password": "Kinderpasswort!!_.ÄÖ",
-        "note 1": "Das Password ist falsch"
-    }
-]
+
 
 class Interface:
     """"
@@ -44,21 +31,26 @@ class Interface:
                     'phonenumber': account.phonenumber
                     })
                     self._stdscr.addstr( 1, 5, "No Accounts found")"""""
-    def showList(self, layer:int, heading:str) -> int:
+    def showList(self, layer:int) -> int:
         self._layer = layer
         self._stdscr.clear()
-        self._stdscr.addstr(0, 0, heading)
         currentPage = self._allPages[layer]
-        for i in range(len(currentPage)):
-            self._stdscr.addstr(i + 1, 5, currentPage[i])
+        length = len(currentPage)
+        self._stdscr.addstr(0, 0, currentPage[0])
+        for i in range(1, length):
+            self._stdscr.addstr(i, 5, currentPage[i])
         self._stdscr.refresh()
         self._lastChooseRow = 1
         curses.curs_set(0)
-        return len(currentPage)
-    def addString(self, previousText:str) ->str:
-        pass
-    def showAccounts(self) -> None:
-        pass
+        return length
+    
+    def showPlattformInfo(self, plattforms: list, offset:int) -> int:
+        length = len(plattforms)
+        for i in range(length):
+            self._stdscr.addstr(i, offset, plattforms[i])
+        self._stdscr.refresh()
+        return length
+            
     def openAccount(number:int) -> None:
         pass
     def __init__(self, accounts: list):
@@ -66,15 +58,20 @@ class Interface:
         self._accounts = accounts
         self._layer = 0
         self._stdscr = None
-        createAccountPage = ["Type your email","","Type your phonenumber","","Type your Password","","Create Account"]
-        loginPage = ["Choose an Account", "Create an Account", "Close this application"]
-        self._headings = ["Welcome to Password Manager", "Choose an Account", "Create an Account"]
-        actionLoginPage = [self.showAccounts(),self.showResponse]
-        self._allPages = [loginPage, accounts, createAccountPage]
+        
+        startPage = ["Welcome to Password Manager","Choose an Account", "Create an Account", "Close this application"]
+        createAccountPage = ["Create your Account","Type your email","","Type your phonenumber","","Type your Password","","Create Account"]
+        inputAccountPage = ["Input your Password for","","LogIn"]
+        self._plattformPage = [["Error occured"],[self.createInfo]]
+        self._allPages = [startPage, accounts, createAccountPage, inputAccountPage]
     
-
-    def showResponse(self): 
-        curses.beep()
+    
+    def createInfo(self) -> list:
+        password = "123212"
+        note =""
+        return [password, note]
+    def noPlattform(self) ->bool:
+        return False
 
     def getData(self, data: list):
         self._data = data
@@ -95,24 +92,45 @@ class Interface:
         stdscr.clear()
 
         
-        lengthOfPage = self.showList(layer = 0, heading = self._headings[0])
+        lengthOfPage = self.showList(layer = 0)
         typeMode = False
         email =""
         phonenumber=""
         password=""
+        masterpassword=""
+        controllpassword="MeineFresse"
         while True:
             chooseRow = self._lastChooseRow
             key = self._stdscr.getkey()
             #self._stdscr.addstr(6, 0, key)
-            if(key == '\n' or key == '\r' or key == curses.KEY_ENTER):#Enter
-                if(self._layer == 2 and typeMode == False and chooseRow <7):#Eingabe Modus für Account erstellen
-                    stdscr.keypad(False)
-                    typeMode = True
-                elif(self._layer == 2 and typeMode == True): #Eingabemodus verlassen
+            if(key == '\n' or key == '\r' or key == curses.KEY_ENTER ):#Enter
+                if( 4>= self._layer >=2 and typeMode == False):#Eingabe Modus aktivieren
+                    if(self._layer == 2 and chooseRow <= 7):
+                        stdscr.keypad(False)
+                        typeMode = True
+                    elif(self._layer == 3 and 0<chooseRow<2):
+                        stdscr.keypad(False)
+                        typeMode = True
+                        
+                    elif(self._layer == 3 and chooseRow == 2):
+                        if(masterpassword == controllpassword):
+                            self._platform = [["Plattformen","Kategeorie Das da","Plattform1","Plattform2","Kategorie2","Plattform3"],
+                                          [self.noPlattform(),self.noPlattform(), self.createInfo(), self.createInfo(), self.noPlattform,self.createInfo()]]
+                            self.showPlattformInfo(self._platform[0], 4) 
+                            lengthOfPage = len(self._platform[0])
+                        else:
+                            masterpassword =""
+                elif(self._layer == 2 and typeMode == False and chooseRow ==7):#Account erstellen
+                    self._stdscr.addstr(9, 0, email)
+                    self._stdscr.addstr(10, 0, phonenumber)
+                    self._stdscr.addstr(11, 0, password)
+                elif(self._layer == 4 and typeMode == False):#Plattform auswählen
+                    continue
+                elif(4>= self._layer >=2 and typeMode == True): #Eingabemodus verlassen
                     stdscr.keypad(True)
                     typeMode = False
-                elif(self._layer == 2 and typeMode == False and chooseRow ==7):#Account erstellen
-                    pass
+                
+                
                         
                 self._stdscr.addstr(lengthOfPage, 0, "Enter")
             elif(key == "KEY_UP"):#Up
@@ -120,19 +138,32 @@ class Interface:
             elif(key == "KEY_DOWN"):#Down
                 chooseRow = chooseRow + 1
             elif(key == "KEY_LEFT"):#Left
-                if self._layer >0:
-                   lengthOfPage = self.showList(layer= self._layer - 1, heading= self._headings[self._layer - 1])#Eine Seite zurück oder
+                if 3 > self._layer >0:
+                   lengthOfPage = self.showList(layer= 0)#Eine Seite zurück oder
+                elif(self._layer == 3):
+                    lengthOfPage = self.showList(layer= 1)#Eine Seite zurück oder
                 else:
                     break#Schließen auf der ersten Seite Speichern der Daten Hier
             elif(key == "KEY_RIGHT"):#Right
                 if(self._layer == 0):#Erste Page
                     if(chooseRow == 1):#Accounts zeigen
-                        lengthOfPage = self.showList(layer = 1,heading=self._headings[1])
+                        lengthOfPage = self.showList(layer = 1)
                     elif(chooseRow == 2):#Account erstellen
-                        lengthOfPage = self.showList(layer= 2, heading= self._headings[2])
+                        lengthOfPage = self.showList(layer= 2)
                     elif(chooseRow == 3):#Schließen Speichern der Daten Hier
                         break
-            elif(typeMode == True):
+                elif(self._layer == 1):#Account Auswählen
+                    lengthOfPage = self.showList(layer = 3)
+                elif(self._layer == 4):
+                    if(self._platform[chooseRow] != False):
+                        plattformInfo = self._platform[1][chooseRow]
+                        self.showPlattform(plattformInfo, offset = 20 )#Zeige Ausgewählte PlatformInfo
+                    else:
+                        continue #Hier dann so Meldung, möchtest du eine neue Kategeorie einfügen oder einen Plattform verschieben
+
+                    #AccountAuswahl mit Account Choose   
+                #Der Ganze Rest der Tastatur dadrunter  
+            elif(typeMode == True and self._layer == 2):#Account Erstelleingaben
                 if((chooseRow == 1 or chooseRow == 2) ):#Email Eingabe
                     if(key == '\b' or key == '\x7f' or key == curses.KEY_BACKSPACE):
                         email = email[:-1]
@@ -151,14 +182,25 @@ class Interface:
                     elif len(key) == 1 and 32 <= ord(key) <= 126:
                         password = password + key
                     self._stdscr.addstr(6, 4, password+ "  ")
+                else:
+                    self._stdscr.addstr(8, 0, "Here could be your Error Message for not knowing which specification have not been reached")
+            elif(typeMode == True and self._layer == 3):#Master Passwort Eingabe
+                if(key == '\b' or key == '\x7f' or key == curses.KEY_BACKSPACE):
+                        masterpassword = masterpassword[:-1]
+                else:
+                        masterpassword = masterpassword + key
+                self._stdscr.addstr(1, 4, masterpassword+ "  ")
+                
+            
+            self._stdscr.refresh()
 
             
                       
         
-            if(chooseRow >lengthOfPage):
+            if(chooseRow >lengthOfPage - 1):
                 chooseRow = 1
             elif(chooseRow <1):
-                chooseRow = lengthOfPage
+                chooseRow = lengthOfPage - 1
             
             self._stdscr.addstr(self._lastChooseRow, 0, "    ")
             self._stdscr.addstr(chooseRow, 0, "--->")
@@ -178,8 +220,6 @@ class Interface:
 
 def main():
     interface = Interface(accounts=accounts)
-    #interface.showResponse()
-    interface.getData(data)
     interface.start()
 
     
