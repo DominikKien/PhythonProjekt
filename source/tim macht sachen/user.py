1f76d797cd62d6de78fae6b897b2304b947c8abe
TimBrielmayer <tim.brielmayer@gmail.com>
2024-09-01 18:04:06 +0200

    user pylint

--- source/tim macht sachen/user.py
+++ source/tim macht sachen/user.py
@@ -1,9 +1,15 @@
+'''saves username and master password and can hash the password'''
+
 from hashlib import sha256
 
 class User:
-    def __init__(self, username: str, master_password: str):
-        self.username = username
-        self.master_password_hash = self.hash_password(master_password)
+    '''defines a user which can be inputed in a Passwordmanager'''
+    def __init__(self, username: str, masterPassword: str) -> None:
+        self.setUsername(username)
+        self.masterPasswordHash = self.hashPassword(masterPassword)
 
-    def hash_password(self, password: str) -> str:
+    def hashPassword(self, password: str) -> str:
         return sha256(password.encode()).hexdigest()
+
+    def setUsername(self, username : str) -> None:
+        self.username = username
