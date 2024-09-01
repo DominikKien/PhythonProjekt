0755a49a83cf77317e63f360bd4fb976e39bf894
JustinLot <justin.lotwin116@gmail.com>
2024-09-01 16:46:14 +0200

    pylint score verbessert

--- source/tim macht sachen/passwordGenerator.py
+++ source/tim macht sachen/passwordGenerator.py
@@ -1,11 +1,28 @@
 """Module for generating passwords"""
-
 import random
 import string
 import math
 import hashlib
 import requests
 
+
+
+def haveIBeenPwned(password : str) -> int:
+
+    """Checks, wether a password has been breached before and how many times it has been breached"""
+
+    url = "https://api.pwnedpasswords.com/range/"
+    breachCount = 0
+    sha1 = hashlib.sha1(password.encode()).hexdigest().upper()
+    result = requests.get(url + sha1[:5])
+    if result.status_code != 200:
+        print("Warning: Cannot check if password has been breached. Please check your internet connection.")
+        return 0
+    hashes = (line.split(':') for line in result.text.splitlines())
+    for suffix, count in hashes:
+        if sha1[5:] == suffix:
+            breachCount = int(count)
+    return int(breachCount)
 
 
 class PasswordGenerator():
@@ -41,22 +58,6 @@ class PasswordGenerator():
         for char in  excludedChars:
             self.chars = self.chars.replace(char, '')
 
-    def haveIBeenPwned(self, password : str) -> int:
-
-        """Checks, wether a password has been breached before and how many times it has been breached"""
-
-        url = "https://api.pwnedpasswords.com/range/"
-        breachCount = 0
-        sha1 = hashlib.sha1(password.encode()).hexdigest().upper()
-        result = requests.get(url + sha1[:5])
-        if result.status_code != 200:
-            print("Warning: Cannot check if password has been breached. Please check your internet connection.")
-            return 0
-        hashes = (line.split(':') for line in result.text.splitlines())
-        for suffix, count in hashes:
-            if sha1[5:] == suffix:
-                breachCount = int(count)
-        return int(breachCount)
 
     def passwordSafety(self, password : str) -> str:
 
@@ -64,46 +65,46 @@ class PasswordGenerator():
 
         bruteForceSafety = int(self.length * math.log2(len(self.chars)))
         #Pwned password
-        if self.haveIBeenPwned(password) > 0:
-            return f"Password has been breached {self.haveIBeenPwned(password)} times."
+        if haveIBeenPwned(password) > 0:
+            return f"Password has been breached {haveIBeenPwned(password)} times."
         #Weak password
-        elif bruteForceSafety < 25:
+        if bruteForceSafety < 25:
             return "weak"
         #OK password
-        elif 25 <= bruteForceSafety < 45:
+        if 25 <= bruteForceSafety < 45:
             return "OK"
         #strong password
-        elif bruteForceSafety >= 45:
+        if bruteForceSafety >= 45:
             return "strong"
         return  "Something went wrong"
-        
-        
-        
+
+
+
 
     def containsEverything(self, testPW : str) -> bool:
 
         """Checks if the password contains at least one of the requested characters"""
-        if(self.useCapitals):
+        if self.useCapitals:
             contains1 = False
         else:
             contains1 = True
-        if(self.useNumbers):
+        if self.useNumbers:
             contains2 = False
         else:
             contains2 = True
-        if(self.useSpecialCharacters):
+        if self.useSpecialCharacters:
             contains3 = False
         else:
             contains3 = True
-        
+
         for char in testPW:
-            if (char in string.ascii_uppercase):
+            if char in string.ascii_uppercase:
                 contains1 = True
-            if (char in string.digits):
+            if char in string.digits:
                 contains2 = True
-            if (char in "%+'-/!,$_"):
+            if char in "%+'-/!,$_":
                 contains3 = True
-        return (contains1 and contains2 and contains3)
+        return contains1 and contains2 and contains3
 
     def generate(self) -> str:
 
