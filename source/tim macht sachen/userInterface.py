f69b758c4b1de36fea697a77529d8a3dc44f0bdc
DominikKien <dominik.kienle.03@gmail.com>
2024-09-01 13:20:31 +0200

    entry daten eingabe/Änderung

--- source/tim macht sachen/userInterface.py
+++ source/tim macht sachen/userInterface.py
@@ -18,6 +18,7 @@ class Interface:
         self.passwordgenerator: PasswordGenerator = PasswordGenerator(6)
         self.entryNumber:int
         self.height:int = -2
+        self.offset = 0 # Offset der Pfeile bei Entrys
 
         self.chooseRow = 1
         self.lengthOfPage = 0
@@ -26,30 +27,22 @@ class Interface:
         self.manager = PasswordManager(self.currentUser)
 
         self.typeMode = False
-        self.userName = ""
-        self.createPassword1 = ""
-        self.createPassword2 = ""
-        self.masterpassword = ""
+        self.userName:str = ""
+        self.createPassword1:str = ""
+        self.createPassword2:str = ""
+        self.masterpassword:str = ""
 
-        self.entry: Dict[str, str] = {
-            "username": "username",
-            "password": "Mein Passwort",
-            "url": "irgenwie Google",
-            "notes": "Leere Notiz",
-            "category": "category Schwarzwaelderkirsch",
-            "created_at": "Heute halt",
-            "history": ["gestern","heute","morgen"]
-        }
-
-        self.plattform: List[str] = ["Netflix", "Instagram", "Kategorie 2", "MyLittlePony"]
-        self.isCategory: List[bool] = [False, False, True, False]
+        self.entryName:str = ""
+        self.url:str = ""
+        self.category:str = ""
+        self.note:str = ""
 
         startPage: List[str] = ["Welcome to Password Manager", "Choose an Account", "Create an Account",  "Close this application"]
         createAccountPage: List[str] = ["Create your Account", "Type your Username", "", "Type your password", "",
                                          "Type your Password again", "", "Create Account", "Generate password"]
         loginAccountPage: List[str] = ["Log into your Account", "Type your Username", "", "Type your password", "", "LogIn"]
         currentAccountPage: List[str] = ["", "Create a new Entry"]
-        newEntryPage: List[str] = ["New Entry", "Type your Name for the plattform", "", "Type the url", "", "Assign a category",
+        newEntryPage: List[str] = ["New Entry", "Type your Name for the plattform", "", "Type the url", "", "might assign a category",
                                     "", "Type the password", "", "Might want to add a short Note?", "", "Save"]
         showPlattformPage: List[str] = ["Entry", "Name of the plattform", "", "url:", "", "category", "", "password", "", "note", "", "Save","created at","" "last edit", ""]
         self._allPages: List[List[str]] = [startPage, createAccountPage, loginAccountPage, currentAccountPage, newEntryPage, showPlattformPage]
@@ -77,7 +70,8 @@ class Interface:
     def showEntry(self, create:bool,height: int, dataList:List = []) -> int:
         """Zeigt den Eintrag an, falls Create True ist wird das Blanko angezeigt. Bei Create false muss Liste übergeben werden"""
         if(create):#Standard DatenListe laden ansonsensten wird übergebene Liste benutzt
-            dataList = self._allPages[5].copy()
+            dataList = self._allPages[4].copy()
+            
         for i, value in enumerate(dataList):
             if i == self.height:
                 self.stdscr.addstr(0, 50, "Please extend your terminal in height")
@@ -89,11 +83,16 @@ class Interface:
         """Fügt die Daten in der richtigen Liste hinzu"""
         dataList:list
         dataList = self._allPages[5].copy() 
-        dataList[2] = data["name"] #Wurde geändert
+        dataList[2] = data["name"]
+        self.entryName = data["name"]
         dataList[4] = data["url"]
+        self.url = data["url"]
         dataList[6] = data["category"]
+        self.category = data["category"]
         dataList[8] = data["password"]
+        self.createPassword1 = data["password"]
         dataList[10] = data["notes"]
+        self.note = data["notes"]
         dataList[13] = data["created_at"]
         dataList.extend(data["history"])
         return dataList
@@ -106,7 +105,8 @@ class Interface:
     def handleEnter(self) ->bool:
         """Kümmert sich um die Eingabe der Enter Taste
         return Wert kann bei False andere Handle Taste aufrufen, Multikeyput"""
-        if 4 >= self.layer >= 1 and not self.typeMode:  # Enter ohne Schreibmodus aktiviert
+        self.stdscr.addstr(0, 30, str(self.chooseRow))
+        if 5 >= self.layer >= 1 and not self.typeMode:  # Enter ohne Schreibmodus aktiviert
             if self.layer == 1 and self.chooseRow < 7:  # Account Erstelleingabe
                 self.stdscr.keypad(False)
                 self.typeMode = True
@@ -127,7 +127,6 @@ class Interface:
                     self.stdscr.addstr(1, 30, "Account erstellt")
                     self.currentUser = User(username=self.userName, master_password=self.createPassword1)  # Account erstellt
                     self._allPages[3][0] = self.userName
-                    self._allPages[3].extend(self.plattform)
                     self.lengthOfPage = self.showList(3)
                     return True
             elif self.layer == 1 and self.chooseRow == 8:
@@ -154,7 +153,11 @@ class Interface:
                 else:
                     self.stdscr.addstr(1, 30, "Wrong Account Name or Password")
                 return True
-        elif 4 >= self.layer >= 1 and self.typeMode:  # Eingabemodus verlassen
+            elif (self.layer == 4 or self.layer == 5) and 1 <= self.chooseRow <= 10:  # Einloggen Eingabe
+                self.stdscr.keypad(False)
+                self.typeMode = True
+                return True
+        elif 5 >= self.layer >= 1 and self.typeMode:  # Eingabemodus verlassen
             self.stdscr.keypad(True)
             self.typeMode = False
             return True
@@ -169,16 +172,19 @@ class Interface:
             elif self.chooseRow == 2:  # Account erstellen
                 self.lengthOfPage = self.showList(layer=1)
             elif self.chooseRow == 3:  # Schließen Speichern der Daten Hier
-                return True
-            elif self.layer == 3:
-                if self.chooseRow == 4:#Eintrag erstellen hier
-                    print("Hier fehlt noch was")                    
+                return True                   
         elif self.layer == 3 and self.lengthOfPage>= self.chooseRow >1: #Einträge anzeigen
-            self.lengthOfPage = self.showEntry(create=False, height= self.height, dataList=self.extractData(self.manager.get_entry(self._allPages[3][self.chooseRow]))) #Plattform anzeigen 
-        elif self.layer == 5:
-            if(self.lengthOfPage >=self.chooseRow > 0):
-                self.entryNumber = 0
-                self.showEntry(entry=self.entryNumber, create=False)
+            self.entryNumber = self.chooseRow
+            self.offset = 36
+            self.layer = 5
+            self.createPassword1 =""
+            self.lengthOfPage = self.showEntry(create=False, height= self.height, dataList=self.extractData(self.manager.get_entry(self._allPages[3][self.chooseRow]))) #Plattform anzeigen
+        elif self.layer == 3 and self.chooseRow == 1:#Eintrag erstellen
+            self.offset = 36
+            self.layer = 4
+            self.createPassword1 =""
+            self.lengthOfPage = self.showEntry(create=True, height=self.height)
+        
             
         return False
     def handleKeyLeft(self)->bool:
@@ -191,6 +197,7 @@ class Interface:
             # Hier auch noch Account schließen
         elif 5 <= self.layer >= 4:
             self.lengthOfPage = self.showList(layer=3)  # Eine Seite zurück
+            self.offset = 0
         else:
             return True  # Schließen auf der ersten Seite Speichern der Daten Hier
         return False
@@ -199,18 +206,17 @@ class Interface:
         """Verifiziert sein Passwort auf Stärke, Benutzer Zeichensatz
             und Wie oft die ersten 5 Hashwerte gepawned wurden
             beim Account erstellen"""
-        self.stdscr.addstr(5, 30, self.createPassword1)
-        self.stdscr.addstr(6, 30, "Strength:")
-        self.stdscr.move(6, 39)
+        self.stdscr.addstr(6, 70, "Strength:")
+        self.stdscr.move(6, 79)
         self.stdscr.clrtoeol()#Befehl zum restliochen Zeileninhalt löschen
-        self.stdscr.addstr(6, 39, self.passwordgenerator.passwordSafety(self.createPassword1))
-        self.stdscr.addstr(7, 30, "Criterias:")
-        self.stdscr.addstr(7, 40, str(self.passwordgenerator.containsEverything(self.createPassword1)))
+        self.stdscr.addstr(6, 79, self.passwordgenerator.passwordSafety(self.createPassword1))
+        self.stdscr.addstr(7, 70, "Criterias:")
+        self.stdscr.addstr(7, 81, str(self.passwordgenerator.containsEverything(self.createPassword1)))
 
     def handleTypemode(self)->None:
         """"Kümmert sich um die Eingabe, Aufruf nur wenn Schreibmodus aktiviert ist"""
         checkPassword = False
-        if  self.layer == 1:  # Account Erstelleingaben
+        if self.layer == 1:  # Account Erstelleingaben
             checkPassword = self.accountCreation()
         elif self.layer == 2:  # Login Eingabe
             if self.chooseRow in (1, 2):
@@ -225,6 +231,39 @@ class Interface:
                 else:
                     self.masterpassword += self.key
                 self.stdscr.addstr(4, 4, self.masterpassword + "  ")
+        elif self.layer == 4 or self.layer ==5:
+            if self.chooseRow in (1, 2):
+                if self.key in( '\b','\x7f',curses.KEY_BACKSPACE):
+                    self.entryName = self.entryName[:-1]
+                else:
+                    self.entryName += self.key
+                self.stdscr.addstr(2, 40, self.entryName + "  ")
+            elif self.chooseRow in (3, 4):
+                if self.key in( '\b','\x7f',curses.KEY_BACKSPACE):
+                    self.url = self.url[:-1]
+                else:
+                    self.url += self.key
+                self.stdscr.addstr(4, 40, self.url + "  ")
+            elif self.chooseRow in (5, 6):
+                if self.key in( '\b','\x7f',curses.KEY_BACKSPACE):
+                    self.category = self.category[:-1]
+                else:
+                    self.category += self.key
+                self.stdscr.addstr(6, 40, self.category + "  ")
+            elif self.chooseRow in (7, 8):
+                checkPassword = True
+                if self.key in( '\b','\x7f',curses.KEY_BACKSPACE):
+                    checkPassword = True
+                    self.createPassword1 = self.createPassword1[:-1]
+                else:
+                    self.createPassword1 += self.key
+                self.stdscr.addstr(8, 40, self.createPassword1 + "  ")
+            elif self.chooseRow in (9, 10):
+                if self.key in( '\b','\x7f',curses.KEY_BACKSPACE):
+                    self.note = self.note[:-1]
+                else:
+                    self.note += self.key
+                self.stdscr.addstr(10, 40, self.note + "  ")
         if checkPassword:
             self.verifyPassword()
 
@@ -270,11 +309,12 @@ class Interface:
         while True:
             self.height, width = self.stdscr.getmaxyx()
             if(oldheight != self.height):
-                self.lengthOfPage = self.showList(self.layer)
-                if(self.layer == 5):
-                    self.showEntry(self.entryNumber, False)
+                if(self.layer < 4):
+                    self.lengthOfPage = self.showList(self.layer)
+                elif(self.layer == 5):
+                    self.showEntry(create=False, height = self.height, dataList=self.extractData(self.manager.get_entry(self._allPages[3][self.entryNumber])))
                 elif(self.layer == 4):
-                    self.showEntry(self.entrynumber, True)
+                    self.showEntry(create=True, height = self.height)
             oldheight = self.height
             #self.chooseRow = self._lastChooseRow
             if self.chooseRow is None:
@@ -304,8 +344,8 @@ class Interface:
             elif self.chooseRow < 1:
                 self.chooseRow = self.lengthOfPage - 1
 
-            self.stdscr.addstr(self._lastChooseRow, 0, "    ")
-            self.stdscr.addstr(self.chooseRow, 0, "--->")
+            self.stdscr.addstr(self._lastChooseRow, self.offset, "    ")
+            self.stdscr.addstr(self.chooseRow, self.offset, "--->")
            #self.stdscr.addstr(5, 0, str(self.layer))
             curses.curs_set(0)
             self._lastChooseRow = self.chooseRow
